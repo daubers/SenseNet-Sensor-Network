7ff8c53f2e1f6c68c95de64a9e1047130a80edb4
Matthew Daubney <matt@daubers.co.uk>
2016-02-28 22:08:13 +0000

    Initial commit

---
+++ esp8266_dht_mqtt.ino
@@ -0,0 +1,93 @@
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h> // https://github.com/knolleary/pubsubclient/releases/tag/v2.3
+#include "DHT.h"
+// ------------- WiFi Settings -------------
+
+const char* ssid = "SenseNet";
+const char* password = "sensenet192";
+
+// ------------- MQTT Settings -------------
+
+char server[] = "mqtt1.sensenet.co.uk";
+char topic_root[] = "sensenet/esp8266/1/";
+char clientId[] = "prototype1";
+
+// ------------- Pin Settings -------------
+
+#define DHT_PIN 2
+#define DHT_POWER_PIN 15
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// ------------- General Settings -------------
+
+int sleep_length = 30000000;
+
+#define MQTT_KEEPALIVE 300000
+
+// End Settings
+
+WiFiClient wifi_client;
+PubSubClient client(server, 1883, callback, wifi_client);
+DHT dht(DHT_PIN, DHTTYPE, 11);
+
+void setup() {
+  // setup wifi
+  if (strcmp (WiFi.SSID(), ssid) != 0) {
+    WiFi.begin(ssid, password);
+  }
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+  }
+  // setup dht
+  pinMode(DHT_POWER_PIN, OUTPUT);
+  digitalWrite(DHT_POWER_PIN, HIGH);
+  delay(100);
+  dht.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (!!!client.connected()) {
+    while (!!!client.connect(clientId)) {
+      delay(500);
+    }
+    Serial.println();
+  }
+
+  //read some values
+
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  while (isnan(t)){
+    delay(2000);
+    h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    t = dht.readTemperature();
+    client.publish(topic_root, "Reading failed, retrying");
+  }
+  //convert these to strings
+  static char temp[9];
+  static char humidity[9];
+  dtostrf(t,5,2,temp);
+  dtostrf(h,5,2,humidity);
+
+  String temp_topic(topic_root);
+  temp_topic = temp_topic + "temperature";
+  client.publish(temp_topic.c_str(), temp); 
+
+  String humidity_topic(topic_root);
+  humidity_topic = humidity_topic + "humidity";
+  client.publish(humidity_topic.c_str(), humidity);
+  
+  //go to sleep
+  digitalWrite(DHT_POWER_PIN, LOW);
+  ESP.deepSleep(sleep_length);
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+}
